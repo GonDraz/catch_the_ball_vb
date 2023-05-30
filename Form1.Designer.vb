00fc2193d16f28f2d257f7f78e5ee308aaabebeb
VŨ NGỌC HẢI HÀ <vungochaiha@gmail.com>
2023-05-31 00:22:47 +0700

    game play

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,22 +20,125 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerSpam = New System.Windows.Forms.Timer(Me.components)
+        Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.Guna2ControlBox1 = New Guna.UI2.WinForms.Guna2ControlBox()
+        Me.Guna2DragControl1 = New Guna.UI2.WinForms.Guna2DragControl(Me.components)
+        Me.imgLogo = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.btnPlay = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Elipse1 = New Guna.UI2.WinForms.Guna2Elipse(Me.components)
+        Me.TimerMove = New System.Windows.Forms.Timer(Me.components)
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.Guna2Panel1.SuspendLayout()
+        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'TimerSpam
+        '
+        Me.TimerSpam.Interval = 1000
+        '
+        'Guna2Panel1
+        '
+        Me.Guna2Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(108, Byte), Integer), CType(CType(148, Byte), Integer))
+        Me.Guna2Panel1.Controls.Add(Me.Guna2ControlBox1)
+        Me.Guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Guna2Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2Panel1.Name = "Guna2Panel1"
+        Me.Guna2Panel1.Size = New System.Drawing.Size(1280, 24)
+        Me.Guna2Panel1.TabIndex = 0
+        '
+        'Guna2ControlBox1
+        '
+        Me.Guna2ControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(CType(CType(139, Byte), Integer), CType(CType(152, Byte), Integer), CType(CType(166, Byte), Integer))
+        Me.Guna2ControlBox1.IconColor = System.Drawing.Color.White
+        Me.Guna2ControlBox1.Location = New System.Drawing.Point(1244, 0)
+        Me.Guna2ControlBox1.Name = "Guna2ControlBox1"
+        Me.Guna2ControlBox1.Size = New System.Drawing.Size(24, 24)
+        Me.Guna2ControlBox1.TabIndex = 0
+        '
+        'Guna2DragControl1
+        '
+        Me.Guna2DragControl1.DockIndicatorTransparencyValue = 0.6R
+        Me.Guna2DragControl1.TargetControl = Me.Guna2Panel1
+        Me.Guna2DragControl1.UseTransparentDrag = True
+        '
+        'imgLogo
+        '
+        Me.imgLogo.BackColor = System.Drawing.Color.Transparent
+        Me.imgLogo.FillColor = System.Drawing.Color.Transparent
+        Me.imgLogo.Image = Global.catch_the_ball_vb.My.Resources.Resources.logo
+        Me.imgLogo.ImageRotate = 0!
+        Me.imgLogo.Location = New System.Drawing.Point(391, 30)
+        Me.imgLogo.Name = "imgLogo"
+        Me.imgLogo.Size = New System.Drawing.Size(517, 199)
+        Me.imgLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.imgLogo.TabIndex = 1
+        Me.imgLogo.TabStop = False
+        '
+        'btnPlay
+        '
+        Me.btnPlay.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnPlay.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnPlay.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnPlay.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnPlay.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.btnPlay.ForeColor = System.Drawing.Color.White
+        Me.btnPlay.Location = New System.Drawing.Point(526, 342)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(180, 45)
+        Me.btnPlay.TabIndex = 2
+        Me.btnPlay.Text = "Play"
+        '
+        'Guna2Elipse1
+        '
+        Me.Guna2Elipse1.BorderRadius = 24
+        '
+        'TimerMove
+        '
+        Me.TimerMove.Interval = 10
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(51, 48)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(145, 37)
+        Me.lblScore.TabIndex = 3
+        Me.lblScore.Text = "Score : 0"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackgroundImage = Global.catch_the_ball_vb.My.Resources.Resources.background
+        Me.ClientSize = New System.Drawing.Size(1280, 720)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.imgLogo)
+        Me.Controls.Add(Me.Guna2Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.KeyPreview = True
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Guna2Panel1.ResumeLayout(False)
+        CType(Me.imgLogo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TimerSpam As Timer
+    Friend WithEvents Guna2Panel1 As Guna.UI2.WinForms.Guna2Panel
+    Friend WithEvents Guna2DragControl1 As Guna.UI2.WinForms.Guna2DragControl
+    Friend WithEvents Guna2ControlBox1 As Guna.UI2.WinForms.Guna2ControlBox
+    Friend WithEvents btnPlay As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents imgLogo As Guna.UI2.WinForms.Guna2PictureBox
+    Friend WithEvents Guna2Elipse1 As Guna.UI2.WinForms.Guna2Elipse
+    Friend WithEvents TimerMove As Timer
+    Friend WithEvents lblScore As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,100 @@
-﻿Public Class Form1
+﻿Imports Guna.UI2.WinForms
+
+
+
+Public Class Form1
+    Private speedMove As Double = 5
+    Private speedSpam As Double = 700
+
+
+    Private basket As PictureBox
+    Private score As Integer = 0
+    Private random As New Random()
+    Private ball As New List(Of PictureBox)()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        basket = New PictureBox()
+        basket.Image = My.Resources.basket
+        basket.SizeMode = PictureBoxSizeMode.StretchImage
+        basket.Size = New Size(200, 112)
+        'basket.BackColor = Color.Transparent
+        basket.Location = New Point((Me.ClientSize.Width - basket.Width) / 2, Me.ClientSize.Height - basket.Height - 10)
+        Me.Controls.Add(basket)
+        Me.DoubleBuffered = True
+    End Sub
+
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        TimerSpam.Interval = Convert.ToInt32(speedSpam)
+        TimerSpam.Start()
+        TimerMove.Start()
+
+        imgLogo.Visible = False
+        btnPlay.Visible = False
+    End Sub
+
+    Private Sub TimerSpam_Tick(sender As Object, e As EventArgs) Handles TimerSpam.Tick
+        Dim newBall As New Guna2CirclePictureBox()
+
+        newBall.Image = My.Resources.ball
+        newBall.SizeMode = PictureBoxSizeMode.StretchImage
+        newBall.Size = New Size(100, 100)
+
+        newBall.Location = New Point(random.Next(0, Me.ClientSize.Width - newBall.Width), 0)
+        Me.Controls.Add(newBall)
+        ball.Add(newBall)
+    End Sub
+
+
+
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        Dim mouseX As Integer = e.X - (basket.Size.Width / 2)
+
+        basket.Left = mouseX
+
+    End Sub
+
+    Private Sub TimerMove_Tick(sender As Object, e As EventArgs) Handles TimerMove.Tick
+
+        For Each item As PictureBox In ball
+            item.Top += Convert.ToInt32(speedMove)
+
+            If item.Bounds.IntersectsWith(basket.Bounds) Then
+                score += 1
+                lblScore.Text = "Score : " & score.ToString()
+                speedMove *= 1.05
+                speedSpam *= 0.98
+                If speedSpam <= 300 Then
+                    speedSpam = 300
+                End If
+                TimerSpam.Interval = Convert.ToInt32(speedSpam)
+                Me.Controls.Remove(item)
+                ball.Remove(item)
+                Exit For
+            End If
+
+            If item.Top >= Me.ClientSize.Height Then
+                EndGame()
+                Exit For
+            End If
+        Next
+    End Sub
+
+
+    Private Sub EndGame()
+        TimerSpam.Stop()
+        TimerMove.Stop()
+        speedMove = 5
+        speedSpam = 700
+
+
+        imgLogo.Visible = True
+        btnPlay.Visible = True
+        For Each item As PictureBox In ball
+            Me.Controls.Remove(item)
+        Next
+        ball.Clear()
+    End Sub
+
 
 End Class
